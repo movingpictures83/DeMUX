e749b0c9f3212769c311169644e580ce5f669c68
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-06-23 16:56:43 -0400

    Initial commit

---
+++ DeMUXPlugin.cpp
@@ -0,0 +1,35 @@
+#include "PluginManager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "DeMUXPlugin.h"
+
+void DeMUXPlugin::input(std::string file) {
+ inputfile = file;
+ std::ifstream ifile(inputfile.c_str(), std::ios::in);
+ while (!ifile.eof()) {
+   std::string key, value;
+   ifile >> key;
+   ifile >> value;
+   parameters[key] = value;
+ }
+}
+
+void DeMUXPlugin::run() {
+   
+}
+
+void DeMUXPlugin::output(std::string file) {
+   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; split_libraries_fastq.py";
+ command += " -i "+parameters["sequences"];
+ command += " -m "+parameters["mapping"];
+ command += " -b "+parameters["barcodes"];
+ if (parameters.count("barcodetype") != 0) {
+    command += " --barcode_type "+parameters["barcodetype"];
+ }
+ command += " -o "+file+"; cp "+file+"/*.txt "+file+"/..; cp "+file+"/*.fna "+file+"/..; export PYTHONPATH=OLDPATH"; 
+ std::cout << command << std::endl;
+
+ system(command.c_str());
+}
+
+PluginProxy<DeMUXPlugin> DeMUXPluginProxy = PluginProxy<DeMUXPlugin>("DeMUX", PluginManager::getInstance());
